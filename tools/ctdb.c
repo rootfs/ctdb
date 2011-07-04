704550edf80031baefd0341712e60aea31b27c5c
Ronnie Sahlberg <ronniesahlberg@gmail.com>
2011-07-05 06:29:00 +1000

    Add log output to wipedb and backupdb CQ S1025379

--- tools/ctdb.c
+++ tools/ctdb.c
@@ -4142,6 +4142,9 @@ done:
 			DEBUG(DEBUG_ERR,("close failed: %s\n", strerror(errno)));
 		}
 	}
+
+	DEBUG(DEBUG_ERR,("Database acked up to %s\n", argv[1]));
+
 	talloc_free(tmp_ctx);
 	return status;
 }
@@ -4584,6 +4587,8 @@ static int control_wipedb(struct ctdb_context *ctdb, int argc,
 		talloc_free(tmp_ctx);
 		return -1;
 	}
+
+	DEBUG(DEBUG_ERR, ("Database wiped.\n"));
 
 	talloc_free(tmp_ctx);
 	return 0;
