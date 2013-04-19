485b32d77972271dd0c5938ab5aa1d657484ae5e
Amitay Isaacs <amitay@gmail.com>
2013-04-19 16:47:32 +1000

    ctdbd: Add --pidfile option
    Default is not to create a pid file.
    
    Signed-off-by: Martin Schwenke <martin@meltin.net>
    Pair-programmed-with: Amitay Isaacs <amitay@gmail.com>
    Reviewed-by: Michael Adam <obnox@samba.org>
    (cherry picked from commit 996e74d3db0c50f91b320af8ab7c43ea6b1136af)
    
    Conflicts:
    	server/ctdb_daemon.c

--- server/ctdb_daemon.c
+++ server/ctdb_daemon.c
@@ -37,6 +37,8 @@ struct ctdb_client_pid_list {
 	struct ctdb_client *client;
 };
 
+const char *ctdbd_pidfile = NULL;
+
 static void daemon_incoming_packet(void *, struct ctdb_req_header *);
 
 static void print_exit_message(void)
@@ -1066,6 +1068,38 @@ static void ctdb_setup_event_callback(struct ctdb_context *ctdb, int status,
 				 tdb_null, NULL, NULL);
 }
 
+static void ctdb_remove_pidfile(void)
+{
+	if (ctdbd_pidfile != NULL && !ctdb_is_child_process()) {
+		if (unlink(ctdbd_pidfile) == 0) {
+			DEBUG(DEBUG_INFO, ("Removed PID file %s\n",
+					   ctdbd_pidfile));
+		} else {
+			DEBUG(DEBUG_WARNING, ("Failed to Remove PID file %s\n",
+					      ctdbd_pidfile));
+		}
+	}
+}
+
+static void ctdb_create_pidfile(pid_t pid)
+{
+	if (ctdbd_pidfile != NULL) {
+		FILE *fp;
+
+		fp = fopen(ctdbd_pidfile, "w");
+		if (fp == NULL) {
+			DEBUG(DEBUG_ALERT,
+			      ("Failed to open PID file %s\n", ctdbd_pidfile));
+			exit(11);
+		}
+
+		fprintf(fp, "%d\n", pid);
+		fclose(fp);
+		DEBUG(DEBUG_INFO, ("Created PID file %s\n", ctdbd_pidfile));
+		atexit(ctdb_remove_pidfile);
+	}
+}
+
 /*
   start the protocol going as a daemon
 */
@@ -1103,8 +1137,7 @@ int ctdb_start_daemon(struct ctdb_context *ctdb, bool do_fork, bool use_syslog, 
 	block_signal(SIGPIPE);
 
 	ctdb->ctdbd_pid = getpid();
-
-
+	ctdb_create_pidfile(ctdb->ctdbd_pid);
 	DEBUG(DEBUG_ERR, ("Starting CTDBD as pid : %u\n", ctdb->ctdbd_pid));
 
 	if (ctdb->do_setsched) {
