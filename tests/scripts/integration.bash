2c97de1aff47e84632f33551de5e35235b0a5eba
Amitay Isaacs <amitay@gmail.com>
2014-01-13 15:16:46 +1100

    tests: Set CTDB_EVENT_HELPER when running with local daemons
    Signed-off-by: Amitay Isaacs <amitay@gmail.com>
    Reviewed-by: Martin Schwenke <martin@meltin.net>
    
    (Imported from commit dd98b9df6651054dabefdf439735042a78cfea2e)

--- tests/scripts/integration.bash
+++ tests/scripts/integration.bash
@@ -15,6 +15,7 @@ if [ -n "$TEST_LOCAL_DAEMONS" ] ; then
     if [ -n "$ctdb_dir" -a -d "${ctdb_dir}/bin" ] ; then
 	PATH="${ctdb_dir}/bin:${PATH}"
         export CTDB_LOCK_HELPER="${ctdb_dir}/bin/ctdb_lock_helper"
+        export CTDB_EVENT_HELPER="${ctdb_dir}/bin/ctdb_event_helper"
     fi
 
     export CTDB_NODES="${TEST_VAR_DIR}/nodes.txt"
