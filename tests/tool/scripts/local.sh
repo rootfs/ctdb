a1f0b5dca579cfb3703bb5192e744c39707de2a4
Martin Schwenke <martin@meltin.net>
2014-03-17 13:42:35 +1100

    tests: Add "ctdb listnodes" and "ctdb xpnn" stub tests
    Tests for xpnn need to implement a stub for ctdb_sys_have_ip().  The
    cheapest way of doing this is to read a fake nodemap using the
    existing code and check if the IP of the "current" node is the one
    being asked about.  However, the fake state initialisation isn't
    currently available to without_daemon commands because it is meant to
    represent daemon state.  However, it can be made available by moving
    the relevant code into a new stub for tevent_context_init().  The stub
    still needs to initialise a tevent context - this can be done by
    calling a lower level function.
    
    Signed-off-by: Martin Schwenke <martin@meltin.net>
    Reviewed-by: Amitay Isaacs <amitay@gmail.com>
    
    (Imported from commit 79d28000043bd463beecaeac47855d3a4970eaf2)

--- tests/tool/scripts/local.sh
+++ tests/tool/scripts/local.sh
@@ -42,6 +42,17 @@ setup_natgw ()
     cat >"$CTDB_NATGW_NODES"
 }
 
+setup_nodes ()
+{
+    debug "Setting up CTDB_NODES"
+
+    # These will accumulate, 1 per test... but will be cleaned up at
+    # the end.
+    export CTDB_NODES=$(mktemp --tmpdir="$TEST_VAR_DIR")
+
+    cat >"$CTDB_NODES"
+}
+
 simple_test ()
 {
     # Most of the tests when the tool fails will have a date/time/pid
